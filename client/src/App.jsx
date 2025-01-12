e94eb5ef31ba19f7a8ed54c6c73ba96b999baeba
scar710813 <unclescar0813@gmail.com>
2025-01-12 19:32:09 +0200

    about page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import './App.css'
 import Navbar from './pages/Navbar.jsx'
 import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
 // import Footer from './pages/Footer.jsx'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Fragment>
       <Navbar />
       <Home />
+      <About />
     </Fragment>
   )
 }
