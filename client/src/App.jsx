72a104ba51b6c46a24a51e80a12c21d2c93d6caa
BlackGhost2693 <blackghost2693@gmai.com>
2025-01-13 02:33:02 +0900

    added service page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import './App.css'
 import Navbar from './pages/Navbar.jsx'
 import Home from './pages/Home.jsx'
+import Services from './pages/Services.jsx'
 // import Footer from './pages/Footer.jsx'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Fragment>
       <Navbar />
       <Home />
+      <Services />
     </Fragment>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
   theme: {
     screens: {
       'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1440px',
-      '3xl': '1536px',
+      'md': '769px',
+      'lg': '1025px',
+      'xl': '1281px',
+      '2xl': '1441px',
+      '3xl': '1537px',
     },
     extend: {
       colors: {
