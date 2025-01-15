34aaed63d6637ebf4100a32299e2ac98e99bf15a
scar710813 <unclescar0813@gmail.com>
2025-01-15 14:52:01 +0200

    'our projects' section add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './pages/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Services from './pages/Services.jsx'
+import Projects from './pages/Projects.jsx'
 // import Footer from './pages/Footer.jsx'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Projects />
     </Fragment>
   )
 }
