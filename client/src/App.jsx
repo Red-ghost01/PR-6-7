e1801a22c51cb4d30d892a8c1cc1403386da8125
BlackGhost2693 <blackghost2693@gmai.com>
2025-01-15 22:00:01 +0900

    added trust page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Services from './pages/Services.jsx'
 import Team from './pages/Team.jsx'
+import Trust from './pages/Trust.jsx'
 // import Footer from './pages/Footer.jsx'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Services />
       <Team />
+      <Trust />
     </Fragment>
   )
 }
