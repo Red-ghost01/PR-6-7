254f2a1025af7a2517afd32c87bd8173476919e2
BlackGhost2693 <blackghost2693@gmai.com>
2025-01-16 22:40:32 +0900

    modified trust page

--- client/src/pages/Trust.jsx
+++ client/src/pages/Trust.jsx
@@ -1,51 +1,73 @@
 import React from "react";
-import CCard from "../components/client-card.jsx";
+import BadgeCard from "../components/client-card.jsx";
 import Url from "../assets/images/logos/instapage-logo-vector.svg";
+import image from "../assets/images/467858946_1317848599119206_8893472275537528835_n.jpg";
 const logos = [
     {
         id: 1,
         name: 'instapage',
-        url: Url,  
+        url: Url,
     },
     {
         id: 2,
         name: 'instapage',
-        url: Url,  
+        url: Url,
     },
     {
         id: 3,
         name: 'instapage',
-        url: Url,  
+        url: Url,
     },
     {
         id: 4,
         name: 'instapage',
-        url: Url,  
+        url: Url,
     },
-     {
+    {
         id: 5,
         name: 'instapage',
-        url: Url,  
-      },    
-      {
+        url: Url,
+    },
+    {
         id: 6,
         name: 'instapage',
         url: Url,
-      },
-      {
+    },
+    {
         id: 7,
         name: 'instapage',
         url: Url,
-      },
-      {
+    },
+    {
         id: 8,
         name: 'instapage',
         url: Url,
-      },
+    },
 
 ];
+const customers = [
+    {
+        id: 1,
+        image: image,
+        title: 'Trusted by customers',
+        description: 'In the result,which is the same as the member of the.',
+    },
+    {
+        id: 2,
+        image: image,
+        title: 'Trusted by customers',
+        description: 'In the result,which is the same as the member of the.',
+    },
+    {
+        id: 3,
+        image: image,
+        title: 'Trusted by customers',
+        description: 'In the result,which is the same as the member of the.',
+    },
+]
 export default function () {
-
+    const title = "Trusted by customers";
+    const description = "In the result,which is the same as the member of the.";
     return (
         <div className=" h-full md:h-screen md:px-[140px] 2xl:pt-24 md:pt-28 px-[70px] bg-[#0A0D13]">
             <div className="flex flex-col items-center justify-center ">
@@ -60,7 +82,11 @@ export default function () {
                         </a>
                     </div>
                 ))}
-                <CCard />
+            </div>
+            <div className="grid grid-cols-1 xl:grid-cols-3 gap-10 py-6">
+                {customers.map((customer) => (
+                    <BadgeCard image={image} title={title} description={description} />
+                ))}
             </div>
         </div>
     )
